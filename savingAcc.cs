7a6ecdb088fbe1fcafac6fc77264ca57b481cf7a
hamim932 <72722819+hamim932@users.noreply.github.com>
2020-12-07 07:34:17 +0600

    Add files via upload

---
+++ savingAcc.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bankapp
+{
+    class Savings : Account
+    {
+        int MaxNumOfTransaction;
+
+        public Savings() : base()
+        {
+            MaxNumOfTransaction = 10;
+        }
+        public Savings(string name, int? accNo, double balance) : base(name, accNo, balance)
+        {
+            MaxNumOfTransaction = 100;
+        }
+        public override void Withdraw(double amount)
+        {
+            if (Balance - amount >= 500 && numOfTransaction <= MaxNumOfTransaction)
+            {
+                Balance -= amount;
+                numOfTransaction++;
+            }
+            else
+            {
+                Console.WriteLine("The Amount is exceeds in the minimum balance");
+            }
+        }
+    }
+}
---
+++ system.cs
@@ -0,0 +1,26 @@
+﻿using using System;
+
+namespace bankapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            
+            Account a0 = new Savings();
+           
+            Account a1 = new Savings("Hamim Hasan", 112233, 10000);
+
+            SpecialSavings s1 = new SpecialSavings(20);
+            
+            Account a2 = new SpecialSavings("Arman Rahman", 445566, 20000, 20);
+
+            Account a3 = new Fixed("Hamim Hasan", 813444, 8000);
+
+            Account a4 = new Overdraft (1000);
+            
+            Overdraft od1 = new Overdraft("Hamim Hasan", 658233, 12066, 4000);
+        }
+    }
+}
