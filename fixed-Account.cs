3273a82c19f1ccd005526ea388ca6f2481f19974
hamim932 <72722819+hamim932@users.noreply.github.com>
2020-12-07 07:36:20 +0600

    Update fixed-Account.cs

--- fixed-Account.cs
+++ fixed-Account.cs
@@ -1,1 +1,30 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bankapp
+{
+    class Fixed : Account
+    {
+
+        DateTime tenure_year = new DateTime(2020, 12, 16);
+        
+        public Fixed(string name, int? accNo, double balance) : base(name,accNo,balance)
+        {
+            this.Name = name;
+            this.AccNo = accNo;
+            this.Balance = balance;
+        }
+        public override void Withdraw(double amount)
+        {
+            if (tenure_year < DateTime.Today && amount <= Balance)
+            {
+                Balance -= amount;
+            }
+            else
+            {
+                Console.WriteLine("Transaction not allow!");
+            }
+        }
+    }
+}
